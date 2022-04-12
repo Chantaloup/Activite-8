66e036a5a9627ee2300bd0aa2cbcf96c14005dbb
Chantal Pépin <102247532+chantpep@users.noreply.github.com>
2022-04-12 16:33:52 -0400

    3 - Ajout script.js pour bouton

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-script.js
+document.querySelector('#button').addEventListener('click', () => {
+    console.log('Message');
+    console.warn('Attention!');
+    console.error(`ERREUR`);
+  });
+  
+  
